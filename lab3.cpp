05b942195b4b7b8894f5b6a64115706cf1502952
TurtleStraws-Lab <gonzalez.moises532776@gmail.com>
2025-02-23 13:52:43 -0800

    final

--- lab3.cpp
+++ lab3.cpp
@@ -4,32 +4,45 @@ using namespace std;
 
 
 int fib(int n){
-int total;    
+int a = 0;
+int b = 1;
+int temp;
     if(n == 0 || n == 1){
-    total = n;
+    return a;
+
     }
+    
     else{
-    total = fib(n-1) + fib(n-2);
+     for (int i = 2; i <= n; i++) {
+        temp = a + b;
+        a = b;
+        b = temp;
+     }
     }
-return total;
+return b;
 }
-int store[] = {};
 
 int main() 
 {
 int n;
 cout << "Enter how many fib numbers you want" <<endl;
 cin >> n;
+clock_t time;
+clock_t Total= clock();
 for(int j = 0; j <= n; j++){
-    clock_t time = clock();
-    //store[j]= fib(j);
+time = clock();
   if(j == 0 || j == 1){
-  cout << " 0" << " + " << j << " = " << fib(j) << " ";
+  cout << "" << j << " " << j << endl;
 }
   else{
-  cout << " " << n << " "<< fib(j-1) << " + " << fib(j-2) << " = " << fib(j) << " ";
+  int fib1 = fib(j-1);
+  int fib2 = fib(j-2);
+  cout << "" << j << " " << fib1 << " + " << fib2 << " = " << (fib1 + fib2) << endl;
   }
-  cout << "time spent: " << (double)(clock() - time)/CLOCKS_PER_SEC << " seconds" <<endl;
+
+cout << "time spent: " << (double)(clock() - time)/CLOCKS_PER_SEC << " seconds" <<endl;
 }
+cout << endl;
+cout << "Total time spent: " << (double)(clock() - Total)/CLOCKS_PER_SEC << " seconds" <<endl;
   return 0;
 }
--- lab3array.cpp
+++ lab3array.cpp
@@ -1,35 +1,42 @@
 #include <iostream>
-#include<time.h>
+#include <ctime>
 using namespace std;
 
-
-int fib(int n){
-int total;    
-    if(n == 0 || n == 1){
-    total = n;
+int fib(int n) {
+    int total;
+    if (n == 0 || n == 1) {
+        total = n;
+    } else {
+        total = fib(n - 1) + fib(n - 2);  
     }
-    else{
-    total = fib(n-1) + fib(n-2);
-    }
-return total;
+    return total;
 }
-int store[] = {};
 
-int main() 
-{
-int n;
-cout << "Enter how many fib numbers you want" <<endl;
-cin >> n;
-for(int j = 0; j <= n; j++){
-    clock_t time = clock();
-    store[j]= fib(j);
-  if(j == 0 || j == 1){
-  cout << " 0" << " + " << j << " = " << store[j] << " ";
+int main() {
+    int n;
+    cout << "Enter how many Fibonacci numbers you want: ";
+    cin >> n;
+    int store[n+1];  
+
+    
+    clock_t Total = clock(); 
+    clock_t time; 
+    for (int j = 0; j <= n; j++) {
+        time = clock();
+        store[j] = fib(j);  
+    cout << "Time spent for " << j << " "<< (double)(clock() - time) / CLOCKS_PER_SEC << " seconds" << endl;
+    }
+  
+    for (int i = 0; i <= n; i++) {
+        if (i == 0 || i == 1) {
+            cout << " " << i << "  " << store[i] << endl;
+        } else {
+            cout << " " << i << " " << store[i-1] << " + " << store[i-2] << " = " << store[i] << endl;
+        }
+    }
+    cout << endl;
+    cout << "Total Time spent: " << (double)(clock() - Total) / CLOCKS_PER_SEC << " seconds" << endl;
+
+    return 0;
 }
-  else{
-  cout << " " << store[j-1] << " + " << store[j-2] << " = " << store[j] << " ";
-  }
-  cout << "time spent: " << (double)(clock() - time)/CLOCKS_PER_SEC << " seconds" <<endl;
-}
-  return 0;
-}
+
